956491e9b9b4463ecca68f4dac7e3e1ca638e3eb
Gonzacs175 <46221344+Gonzacs175@users.noreply.github.com>
2019-12-12 12:47:04 +0100

    hecho hasta lector
    a veces funciona

--- main.c
+++ main.c
@@ -14,14 +14,18 @@
 sem_t mutex_Buffer;
 sem_t hayDato;
 sem_t hayEspacio;
+sem_t consumListo;
+sem_t mutexLector;
 
 int *buffer1;
+
 
 int numMaxPosible=3000000;
 
 int tamBuffer;
 int numConsumidores;
 char *nombreFich;
+FILE *fichSalida;
 
 struct valoresConsumidor{
     //Creación de estructura con la que trabajaremos en cada Hilo consumidor
@@ -29,17 +33,19 @@ struct valoresConsumidor{
     int maximodato;
     int minimodato;
     int sumatotal;
-    int media;
-    int mediana;
-    int cuartil;
+    int rango_up;
+    int rango_down;
+    float media;
+    bool finish;
 };
+
+struct valoresConsumidor *valores;
 
 void *productor(void *args){
     char *fichero;
     fichero=nombreFich;
     int j=0;
     int dato=0;
-    char palabra[10];
     char caracter;
     FILE *file;
     //"C:\\Users\\CLION\\untitled\\numeros.dat"
@@ -53,12 +59,13 @@ void *productor(void *args){
         caracter=fgetc(file);
 
         //if((strcmp(palabra,"d")==0)||(strcmp(palabra,"d")==0))
-        if(((isdigit((int)caracter))==0)&&((isdigit((int)caracter))!=0)&&(caracter!='\n')){
+        if(((isdigit((int)caracter))==0)&&(caracter!='\n')){
+
             //quitamos los caracteres problematicos
         }else{
 
             int numero = caracter-'0';    //para transformar de char a int
-            if((caracter=='\n')){
+            if(caracter=='\n'){
                 //si es el final de linea dividimos entre 10 para tener el numero que hay en esa linea y lo guardamos en nuestro buffer
                 dato=dato/10;
                 if(dato!=0){
@@ -79,27 +86,33 @@ void *productor(void *args){
         }
 
     }
+    sem_wait(&hayEspacio);
+    buffer1[j]=-1;
+    sem_post(&hayDato);
+
 
     // for para ver el contenido del buffer
-  /*  for (int j=0;j<tamBuffer;j++){
-        printf("%d\n",buffer1[j]);
-    }*/
+    /*  for (int j=0;j<tamBuffer;j++){
+          printf("%d\n",buffer1[j]);
+      }*/
     fclose(file);
     pthread_exit(0);
 }
 void* consumidor(void* arg){
     int *id = (int *)arg;
+    int ident=*id;
     int rango= numMaxPosible/(numConsumidores);
     int rango_down=*id*rango;
     int rango_up=((*id +1)*rango )-1;
-
+    struct valoresConsumidor valCons;
     int suma=0;
     int  max=0;
     int  min=3000000;
-    int media=0;
+    float media=0;
     int i=0;
     int datob=0;
     int numeroDatosLeidos=0;
+    valCons.finish=false;
     while(true){
         sem_wait(&hayDato);
         //para saber si el valor tratado esta dentro de nuestro rango
@@ -107,8 +120,10 @@ void* consumidor(void* arg){
         datob=buffer1[i];
         sem_post(&mutex_Buffer);
 
-        if(datob== '\0')
+        if(datob== -1) {
+            sem_post(&hayDato);
             break;
+        }
 
         if((rango_down<=datob)&&(datob<=rango_up)){
             numeroDatosLeidos++;
@@ -128,16 +143,71 @@ void* consumidor(void* arg){
     }
     media=suma/numeroDatosLeidos;
 
-
-    printf("El max del %i es:%d\n",id,max);
-    printf("La suma del %i es:%d\n",id,suma);
-    printf("La media del %i es:%d\n",id,media);
-    printf("El min del %i es:%d\n",id,min);
+    valCons.sumatotal=suma;
+    valCons.maximodato=max;
+    valCons.minimodato=min;
+    valCons.media=media;
+    valCons.rango_down=rango_down;
+    valCons.rango_up=rango_up;
+    valCons.numerodatos=numeroDatosLeidos;
+    valCons.finish=true;
+    valores[ident]=valCons;
+    sem_post(&consumListo);
     pthread_exit(0);
 }
+
+void *lector(void* args){
+    int numleidos=0;
+    if(fichSalida== NULL) {
+        printf("No se ha podido encontrar el fichero");
+        exit(-1);
+    }
+    struct valoresConsumidor valLector;
+    while(true) {
+
+        sem_wait(&consumListo);
+
+
+        for (int i = 0; i < numConsumidores; i++) {
+            sem_wait(&mutexLector);
+            valLector = valores[i];
+            sem_post(&mutexLector);
+            if (valLector.finish == true) {
+                fprintf(fichSalida, "====== RANGO DEL HILO %d [%d]-[%d]\n", i, valLector.rango_down,
+                        valLector.rango_up);
+                fprintf(fichSalida, "El numero de datos del hilo %d es: %d\n", i, valLector.numerodatos);
+                fprintf(fichSalida, "El maximo del hilo %d es:%d\n", i, valLector.maximodato);
+                fprintf(fichSalida, "El minimo del hilo %d es:%d\n", i, valLector.minimodato);
+                fprintf(fichSalida, "La suma total del hilo  %d es:%d\n", i, valLector.sumatotal);
+                fprintf(fichSalida, "La media del hilo %d es:%12.6f\n", i, valLector.media);
+                fprintf(fichSalida, "\n\n");
+                valLector.finish = false;
+
+                fflush(fichSalida);
+
+                numleidos++;
+            }
+
+        }
+
+
+        if (numleidos == numConsumidores){
+            break;
+        }
+
+    }
+    pthread_exit(0);
+}
+
 int main(int argc, char* argv[]) {
     char *fichero= argv[1];
     nombreFich =fichero;
+    char *ficheroSal= argv[2];
+    char *ficherSalida= ficheroSal;
+
+    fichSalida=fopen(ficherSalida,"w");
+
+
     sscanf(argv[3],"%d",&tamBuffer);
     sscanf(argv[4],"%d",&numConsumidores);
 
@@ -153,15 +223,19 @@ int main(int argc, char* argv[]) {
 
     //Memoria dinámica,
     buffer1=(int*)malloc(tamBuffer*sizeof(int));
+    valores=(struct valoresConsumidor*)malloc(numConsumidores*sizeof(struct valoresConsumidor));
+
 
     //iniciador hilo
     pthread_t productorhilo;
     pthread_t consumidorhilo[numConsumidores];
+    pthread_t lectorhilo;
     //iniciador de semaforo, esto me lo dijo el profe así que será así.
     sem_init(&hayEspacio,0,tamBuffer);
     sem_init(&hayDato,0,0);
     sem_init(&mutex_Buffer,0,1);
-
+    sem_init(&consumListo,0,0);
+    sem_init(&mutexLector,0,1);
     //creador hilo
 
     int id[numConsumidores];
@@ -169,6 +243,7 @@ int main(int argc, char* argv[]) {
         id[i]=i;
     }
     pthread_create(&productorhilo,NULL,productor,(void*)NULL);
+    pthread_create(&lectorhilo,NULL,lector,(void*)NULL);
 
 
     for(int i=0;i<numConsumidores;i++){
@@ -176,6 +251,7 @@ int main(int argc, char* argv[]) {
     }
 
     pthread_join(productorhilo,NULL);
+    pthread_join(lectorhilo,NULL);
     for(int i=0;i<numConsumidores;i++){
         pthread_join(consumidorhilo[i],NULL);
     }
